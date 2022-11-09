cab3dbb4d60f56d89a22eb9078d3a75094283f6b
Óscar García <oscar.garcia@makingscience.com>
2022-11-09 13:15:54 +0100

    Copy the example from the class slides

--- main.c
+++ main.c
@@ -1,7 +1,22 @@
-#include<stdio.h>
+#include <stdio.h>
+#include <pthread.h>
+#include <unistd.h>
+
+void* threadfun(void *arg) {
+    int thread_num = *((int *)arg);
+    printf("I’m the thread with id -> %d\n", thread_num);
+    pthread_exit(0);
+}
+
 
 int main() {
     printf("Hello world!\n");
-    return(0);
+    pthread_t thread1, thread2;
+    int id1 = 1, id2 = 2;
+    pthread_create(&thread1, NULL, threadfun, &id1);
+    pthread_create(&thread2, NULL, threadfun, &id2);
+    pthread_join(thread1, NULL);
+    pthread_join(thread2, NULL);
+    return 0;
 }
 
