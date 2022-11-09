e6e839d1885b63edff5dd962a9f1a7f223b21e4e
Óscar García <oscar.garcia@makingscience.com>
2022-11-09 13:19:38 +0100

    Threads are painting funny symbols

--- main.c
+++ main.c
@@ -4,7 +4,18 @@
 
 void* threadfun(void *arg) {
     int thread_num = *((int *)arg);
+    int i;
+
     printf("I’m the thread with id -> %d\n", thread_num);
+    for(i=0;i<100;i++) {
+        if (thread_num==1) {
+            printf("O\n");
+            sleep(1);
+        } else {
+            printf("o\n");
+            sleep(2);
+        }
+    }
     pthread_exit(0);
 }
 
